61667396155841bdcc71e02a6025549747ec8f8d
NelsonWang <wangnelson2@gmail.com>
2023-11-08 21:52:48 -0500

    dashboard routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+// import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  // {
+  //   path: 'dashboard',
+  //   canActivate: [AuthGuard],
+  //   component: DashboardComponent,
+  // },
+  // LAZY LOADING
   {
     path: 'dashboard',
-    canActivate: [AuthGuard],
-    component: DashboardComponent,
+    canActivate: [AuthGuard], // Protect this route with AuthGuard
+    loadChildren: () =>
+      import('./dashboards/dashboards.module').then((m) => m.DashboardsModule),
   },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
---
+++ src/app/dashboards/dashboard/dashboard.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../auth.services';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+
+export class DashboardComponent {
+  user = this.authService.authUser$;
+  constructor(private authService: AuthService) {}
+
+  logout(): void {
+    this.authService.logout();
+  }
+}
+
