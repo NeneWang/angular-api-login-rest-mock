bf205f752e3a84ad818d9371e4440558df51d0fd
NelsonWang <wangnelson2@gmail.com>
2023-11-08 21:10:05 -0500

    adding some basic forms

--- src/app/auth.services.ts
+++ src/app/auth.services.ts
@@ -1,22 +1,57 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private isAuthenticated = false;
+  constructor(private router: Router, private httpClient: HttpClient) { }
 
-  login(): void {
+  private isAuthenticated = false;
+  
+  private _authUser$ = new BehaviorSubject<any | null>(null);
+
+  public authUser$ = this._authUser$.asObservable();
+
+  login(payload: LoginPayload): void {
     this.isAuthenticated = true;
+    this.httpClient
+      .get<any[]>(
+        `http://localhost:3000/users?email=${payload.email}&password=${payload.password}`
+      )
+      .subscribe({
+        next: (response) => {
+          if (!response.length) {
+            alert('Usuario o contrasena invalidos');
+          } else {
+            const authUser = response[0];
+            this._authUser$.next(authUser);
+            localStorage.setItem('token', authUser.token);
+            this.router.navigate(['/dashboard/home']);
+          }
+        },
+        error: (err) => {
+          alert('Error de conexion');
+        },
+      });
+    this.router.navigate(['/dashboard']);
+
   }
 
   logout(): void {
     this.isAuthenticated = false;
+    this.router.navigate(['/login']);
   }
 
   isAuthenticatedUser(): boolean {
     return this.isAuthenticated;
   }
+}
+
+
+export interface LoginPayload {
+  email: string | null;
+  password: string | null;
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.services';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,8 +9,21 @@ import { AuthService } from '../auth.services';
 })
 export class LoginComponent {
   constructor(private authService: AuthService) {}
+  emailControl = new FormControl('', [Validators.required, Validators.email]);
+  passwordControl = new FormControl('', [Validators.required]);
 
+  loginForm = new FormGroup({
+    email: this.emailControl,
+    password: this.passwordControl,
+  });
+
+  
   login(): void {
-    this.authService.login();
+    // this.authService.login();
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+    } else {
+      this.authService.login(this.loginForm.getRawValue());
+    }
   }
 }
